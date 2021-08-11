00ce5f52b0cecec86f5bd848337666327befd4dc
Luka Franjić <lukafranjic94@gmail.com>
2021-08-11 23:07:55 +0200

    Add resource and database errors to shared constatns

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,3 +1,7 @@
 // Put shared constants here
 
-export const paramMissingError = 'One or more of the required parameters was missing.';
+export const paramMissingError: string =
+  "One or more of the required parameters was missing.";
+export const resourceNotFoundError: string =
+  "The requested resource was not found.";
+export const databaseQueryError: string = "The database has run into an error.";
